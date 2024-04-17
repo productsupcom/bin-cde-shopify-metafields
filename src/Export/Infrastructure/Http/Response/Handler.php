0e6657023b77aef34b1dc4887df2eb7750e8259a
khelmrich <kamil.helmrich@productsup.com>
2024-04-17 11:13:07 +0200

    wip: add response handler

---
+++ src/Export/Infrastructure/Http/Response/Handler.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Productsup\BinCdeShopifyMetafields\Export\Infrastructure\Http\Response;
+
+use Productsup\BinCdeShopifyMetafields\Export\Application\Feedback\Feedback;
+use Productsup\DK\Connector\Application\Output\Feedback\FeedbackHandler;
+
+class Handler
+{
+    public function __construct(
+        private FeedbackHandler $feedbackHandler
+    ) {
+    }
+    public function handle(array $responseData, array $data, string $metafield): void
+    {
+        if (isset($responseData['data']['metafieldsSet']['userErrors'])) {
+            foreach ($responseData['data']['metafieldsSet']['userErrors'] as $error) {
+                $this->feedbackHandler->handle(new Feedback($data, $metafield, $error['message'].' '.$error['code']));
+            }
+        }
+    }
+}
