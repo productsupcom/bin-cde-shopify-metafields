0ad5e9acb9925ec68ed79b16147457962e05c2db
khelmrich <kamil.helmrich@productsup.com>
2024-04-23 15:41:45 +0200

    wip: debug

--- src/Export/Application/Handler/MetafieldHandler.php
+++ src/Export/Application/Handler/MetafieldHandler.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Productsup\BinCdeShopifyMetafields\Export\Application\Handler;
 
 use Productsup\BinCdeShopifyMetafields\Export\Domain\Http\Uploader\MetafieldUploaderInterface;
+use Productsup\DK\Connector\Application\Logger\ConnectorLogger;
 use Productsup\DK\Connector\Application\Progress\CountableProgressHandler;
 use Traversable;
 
@@ -13,12 +14,15 @@ final readonly class MetafieldHandler
     public function __construct(
         private MetafieldUploaderInterface $uploader,
         private CountableProgressHandler $progressHandler,
+        private ConnectorLogger $logger,
     ) {
     }
 
     public function handle(Traversable $feed): void
     {
         foreach ($feed as $item) {
+            $this->logger->debug('Sending item to the uploader');
+            $this->logger->debug(json_encode($item, JSON_PRETTY_PRINT));
             $this->uploader->sendBuffered($item);
             $this->progressHandler->progress($this->uploader->getSentItemsCounter());
         }
--- src/Export/Infrastructure/Http/Response/ResponseHandler.php
+++ src/Export/Infrastructure/Http/Response/ResponseHandler.php
@@ -5,17 +5,20 @@ declare(strict_types=1);
 namespace Productsup\BinCdeShopifyMetafields\Export\Infrastructure\Http\Response;
 
 use Productsup\BinCdeShopifyMetafields\Export\Application\Feedback\Feedback;
+use Productsup\DK\Connector\Application\Logger\ConnectorLogger;
 use Productsup\DK\Connector\Application\Output\Feedback\FeedbackHandler;
 
 final readonly class ResponseHandler
 {
     public function __construct(
-        private FeedbackHandler $feedbackHandler
+        private FeedbackHandler $feedbackHandler,
+        private ConnectorLogger $logger,
     ) {
     }
     public function handle(array $responseData, array $data, string $metafield): void
     {
         $errors = $responseData['data']['metafieldsSet']['userErrors'] ?? [];
+        $this->logger->debug(json_encode($responseData, JSON_PRETTY_PRINT));
 
         foreach ($errors as $error) {
             $this->feedbackHandler->handle(new Feedback($data, $metafield, "{$error['message']} {$error['code']}"));
